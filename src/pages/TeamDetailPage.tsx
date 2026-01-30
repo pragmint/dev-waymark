9ae0ffc6692bf40ae8391ea06d68d074b406477a
Dave Moore <dave@pragmint.com>
2026-01-30 12:36:04 -0800

    Make some copy tweaks to team detail page

--- src/pages/TeamDetailPage.tsx
+++ src/pages/TeamDetailPage.tsx
@@ -150,10 +150,10 @@ export const TeamDetailPage: FC<TeamDetailPageProps> = ({
         </section>
 
         <section class="team-section">
-          <h2>Active Experiments</h2>
+          <h2>Experiments</h2>
           <p class="section-intro">
-            Engineering practices the team is experimenting with to improve their delivery
-            performance.
+            Engineering practices the team is considering experimenting with to improve
+            their delivery performance.
           </p>
           {experiments && experiments.length > 0 ? (
             <div class="experiment-cards">
